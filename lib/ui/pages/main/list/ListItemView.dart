5491111da4d6870df3f7d1ef926f760ede2f6656
王欣 <wangxin102@58.com>
2020-11-20 14:44:39 +0800

    分类页面添加圆角

--- lib/ui/pages/main/list/ListItemView.dart
+++ lib/ui/pages/main/list/ListItemView.dart
@@ -1,5 +1,7 @@
 import 'dart:ui';
 
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_redux_pratise/model/list/RankingItemModel.dart';
 
@@ -16,31 +18,42 @@ class ListItemView extends StatelessWidget {
 
   Widget getView() {
     return Container(
-      padding: EdgeInsets.only(left: 10,right: 10,top: 10),
-      child: Container(
-        child: Column(
+      color: Colors.white,
+      padding: EdgeInsets.all(8),
+      child: new Material(
+        shadowColor: Colors.grey[200],
+        elevation: 4,
+        borderRadius: BorderRadius.all(Radius.circular(8)),
+        color: Colors.white,
+        clipBehavior: Clip.hardEdge,
+        child: Stack(
+          fit: StackFit.expand,
+          alignment: Alignment.center,
           children: [
-            Container(
-              child: PhysicalModel(
-                color: Colors.transparent, //设置背景底色透明
-                borderRadius: BorderRadius.only(topLeft: Radius.circular(10.0),topRight: Radius.circular(10.0)),
-                clipBehavior: Clip.antiAlias, //注意这个属性
-                child: Image(
-                  image: NetworkImage(
-                    this.rankingItemModel.cover,
-                  ),
-                  fit: BoxFit.fill,
+            Positioned(
+              child: CachedNetworkImage(
+                imageUrl: rankingItemModel.cover,
+                fit: BoxFit.cover,
+              ),
+              top: 0,
+              left: 0,
+              bottom: 25,
+              right: 0,
+            ),
+            Positioned(
+              child: Center(
+                child: Text(
+                  rankingItemModel.sortName,
+                  style:
+                      TextStyle(fontSize: 12),
                 ),
               ),
+              height: 25,
+              left: 0,
+              bottom: 0,
+              right: 0,
             ),
-            // Image.network(this.rankingItemModel.cover,fit: BoxFit.fill,),
-            Text(rankingItemModel.sortName),
           ],
-        ),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.all(Radius.circular(10)),
-          border: new Border.all(color: Colors.grey.shade300, width: 1),
         ),
       ),
     );
